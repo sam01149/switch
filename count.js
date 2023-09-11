e0eff03a89a65134c1a4487568b46ee1302484d1
vigan <91748878+wibutolol@users.noreply.github.com>
2023-09-11 15:13:34 +0700

    Add files via upload

---
+++ count.js
@@ -0,0 +1,10 @@
+var btn = document.getElementById('btn')
+var toggle = document.getElementsByClassName("button-box")
+function leftClick(){
+    btn.style.left='0px';
+
+}
+function rightClick(){
+    btn.style.left='110px';
+    
+}
